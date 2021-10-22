ae886db33dd76b083d33e40e9487ff6a7ac56dda
Benjamin <benjaminrenny@gmail.com>
2021-10-22 12:23:36 +0100

    corrections

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Button, IconButton } from "@material-ui/core";
-import { AddIcon, InboxIcon, StarIcon, LabelImportantIcon, NearMeIcon, NoteIcon, ExpandMoreIcon, PersonIcon, DuoIcon, PhoneIcon } from "@material-ui/icons";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import AddIcon from "@material-ui/icons/Add";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import { useDispatch } from "react-redux";
 import { openSendMessage } from '../../features/mailSlice';
