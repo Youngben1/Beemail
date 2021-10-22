58a12277fe4ddfa15123186a2d4efe65bbf0b271
Benjamin <benjaminrenny@gmail.com>
2021-10-22 12:39:41 +0100

    final

--- src/components/mail/Mail.js
+++ src/components/mail/Mail.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { ArrowBackIcon, MoveToInboxIcon, ErrorIcon, DeleteIcon, EmailIcon, WatchLaterIcon, CheckCircleIcon, LabelImportantIcon, MoreVertIcon, UnfoldMoreIcon, PrintIcon, ExitToAppIcon } from "@material-ui/icons";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { IconButton } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { selectOpenMail } from "../../features/mailSlice";
